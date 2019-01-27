25ceebbd715f9eebef182cb3d53ef023e41a09de
Abhishek <abhishek.omex@gmail.com>
2019-01-27 20:54:47 +0530

    added mail functionality

--- Node/controllers/mailController.js
+++ Node/controllers/mailController.js
@@ -6,11 +6,16 @@ const check = require('./../libs/checkLib')
 const response = require('./../libs/responseLib')
 
 
-let sendMail = (req, res) => {
-
+let sendMail = (req,res) => {
+   
+    let stri = JSON.parse(Object.keys(req.body)[0])
+    let name = stri.name
+    let mobileNumber = stri.phone
+    let emailId = stri.email
+    let message = stri.message
     // async..await is not allowed in global scope, must use a wrapper
     async function main() {
-
+        console.log(name)
         // Generate test SMTP service account from ethereal.email
         // Only needed if you don't have a real mail account for testing
         let account = await nodemailer.createTestAccount();
@@ -21,27 +26,26 @@ let sendMail = (req, res) => {
             port: 587,
             secure: false, // true for 465, false for other ports
             auth: {
-                //generated ethereal password
+               // generated ethereal password
             }
         });
 
         // setup email data with unicode symbols
         let mailOptions = {
             from: 'awzing.help@gmail.com', // sender address
-            to: "abhishek.omex@gmail.com", // list of receivers
-            subject: "Hello ✔", // Subject line
-            text: "Hello world?", // plain text body
-            html: "<b>Hello world?</b>" // html body
+            to: "abhishek.omex@gmail.com,avinash.kumar520@gmail.com", // list of receivers
+            subject: "Contact-Us", // Subject line
+            text: name + ' ' +  'wants to contact you with mobilenumber: ' + mobileNumber +  
+            'and email: ' + emailId + 'with message: ' + message
         };
-
+        console.log(mailOptions)
         // send mail with defined transport object
         let info = await transporter.sendMail(mailOptions)
 
-        console.log("Message sent: %s", info.messageId);
+        //console.log("Message sent: %s", info.messageId);
+        res.send(Object.keys(req.body)[0])
         // Preview only available when sending through an Ethereal account
         console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-
-        res.send('email sent')
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     }
